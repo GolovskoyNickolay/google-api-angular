9e254c30bef63acc7ead63dfb34cb850a27cdb42
PowerfulPaw <golovskoy.nickolay@gmail.com>
2017-07-17 22:19:26 +0300

    'commit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Geocoding } from './services/geocoding.service';
-import { Location } from './location';
+
 
 @Component({
   selector: 'app-root',
@@ -11,25 +11,48 @@ import { Location } from './location';
 export class AppComponent {
   lat: number = 51.678418;
   lng: number = 7.809007;
+  modal:boolean = false;
   model:any;
-  item:any;
-  arrLocations:any = JSON.parse(localStorage.getItem('arrLocations')) || [];
+  item:any = {};
+  arr:any = JSON.parse(localStorage.getItem('arrItems')) || [];
+
+
+
   constructor(private geo: Geocoding){
     this.model = {
-      address:''
+      address:'',
+      name:''
     }
   }
 
   getGeo(){
-    this.geo.getGecoding(this.model.address).subscribe((data)=>{
+    this.geo.getGecoding(JSON.stringify(this.model.address)).subscribe((data)=>{
+      this.item = {};
       this.lat = data.results[0].geometry.location.lat;
       this.lng = data.results[0].geometry.location.lng;
-      this.item = new Location(this.lat,this.lng);
-      console.log(this.arrLocations);
-      console.log(this.item);
-      this.arrLocations = this.arrLocations.push(JSON.stringify(this.item));
-      localStorage.setItem('arrLocations', JSON.stringify(this.arrLocations));
+      this.item.lat = this.lat;
+      this.item.lng = this.lng;
+      this.modal = true;
     });
+  }
+  saveGeo(){
+        this.item.name = this.model.name;
+    console.log(this.item);
+        this.arr.push(this.item);
+    console.log(this.arr);
+        localStorage.setItem('arrItems',JSON.stringify(this.arr));
+        this.modal = false;
+  }
+  hideModal(){
+    this.modal = false;
+  }
+  reverseGeo(lat,lng){
+        this.lat = lat;
+        this.lng = lng;
+  }
+  remove(i){
+        this.arr.splice(i,1);
+        localStorage.setItem('arrItems',JSON.stringify(this.arr));
   }
 
 }
--- src/app/location.ts
+++
@@ -1,8 +0,0 @@
-export class Location{
-        lat: number;
-        lng: number;
-    constructor(_lat:number, _lng:number){
-      this.lat = _lat;
-      this.lng = _lng;
-    }
-}
